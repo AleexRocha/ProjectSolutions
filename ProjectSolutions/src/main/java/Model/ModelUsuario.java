08b50eaa8bcb9ba0cbe4b425b9962ee5e2a84a9f
Deborah <Deborah@Guilherme.GUILHERME>
2019-04-05 10:03:50 -0300

    Construindo Model. Projeto p.i

--- ProjectSolutions/src/main/java/Model/ModelUsuario.java
+++ ProjectSolutions/src/main/java/Model/ModelUsuario.java
@@ -1,14 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
 /**
  *
- * @author Alexsander Rocha
+ * @author guilherme.rsvieira
  */
-public class ModelTeste {
+ public class ModelUsuario {
+
+    private int codUser;
+    private String nome;
+    private String email;
+    private String senha;
+    private String filial;
+    private String setor;
+
+    public ModelUsuario(int codUser, String nome, String email, String senha, String filial, String setor) {
+        this.codUser = codUser;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.filial = filial;
+        this.setor = setor;
+    }
+     
     
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+    
+    public String getFilial() {
+        return filial;
+    }
+
+    public void setFilial(String filial) {
+        this.filial = filial;
+    }
+
+    public int getCodUser() {
+        return codUser;
+    }
+
+    public void setCodUser(int codUser) {
+        this.codUser = codUser;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+   
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+
+   
+    
+
 }
