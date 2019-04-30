d7c3924ab91b36e063b134e008af4e9dea75b2d4
Guilherme <guisil_ps@hotmail.com>
2019-04-30 10:26:07 -0300

    Ajustando set Time Zone direto no codigo

--- ProjectSolutions/src/main/java/DAO/Database.java
+++ ProjectSolutions/src/main/java/DAO/Database.java
@@ -22,7 +22,7 @@ public class Database {
         usuario = "root";
         senha = "";
         nomeDB = "TADES_BCD";
-        url = "jdbc:mysql://127.0.0.1:3306/" + nomeDB;
+        url = "jdbc:mysql://127.0.0.1:3306/" + nomeDB + "?serverTimezone=UTC";
     }
 
     public String getDriver() {
